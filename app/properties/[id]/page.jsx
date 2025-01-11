f158e777fd375c2fcb96a31f5fba0585cfe2f6f5
Namit Malasi <namitmalasi@gmail.com>
2025-01-11 13:38:23 +0530

    adding property details component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,3 +1,4 @@
+import PropertyDetails from "@/components/PropertyDetails";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
@@ -24,7 +25,7 @@ const PropertyPage = async ({ params }) => {
       <section className="bg-blue-50">
         <div className="container m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-            {/* Property Info           */}
+            <PropertyDetails property={property} />
           </div>
         </div>
       </section>
